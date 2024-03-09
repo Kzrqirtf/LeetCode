4b1d14e9831595b96d41ced1f8c08b53ef25bdf6
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-03-09 14:13:38 +0530

    Time: 100 ms (25.70%) | Memory: 69.1 MB (18.41%) - LeetSync

--- Leetcode150/2634-minimum-common-value/minimum-common-value.cpp
+++ Leetcode150/2634-minimum-common-value/minimum-common-value.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-         int len2 = nums2.size() - 1;
+        //  int len2 = nums2.size() - 1;
+        // for (int num : nums1) {
+        //     int l = 0, r = len2;
+        //     while (l <= r) {
+        //         int m = l + (r - l) / 2;
+        //         if (nums2[m] == num) {
+        //             return num;
+        //         } else if (nums2[m] < num) {
+        //             l = m + 1;
+        //         } else {
+        //             r = m - 1;
+        //         }
+        //     }
+        // }
+        // return -1;   
+        unordered_set<int> nums2Set(nums2.begin(), nums2.end());
+
         for (int num : nums1) {
-            int l = 0, r = len2;
-            while (l <= r) {
-                int m = l + (r - l) / 2;
-                if (nums2[m] == num) {
-                    return num;
-                } else if (nums2[m] < num) {
-                    l = m + 1;
-                } else {
-                    r = m - 1;
-                }
+            if (nums2Set.find(num) != nums2Set.end()) {
+                return num;
             }
         }
-        return -1;   
+
+        return -1;
         
     }
 };
