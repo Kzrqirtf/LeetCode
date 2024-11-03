728db59a99f474dd5ef41c40c2c0acff641d0350
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-11-03 22:56:09 +0530

    Time: 2 ms (83.87%) | Memory: 14.5 MB (20.55%) - LeetSync

--- Leetcode150/1-two-sum/two-sum.cpp
+++ Leetcode150/1-two-sum/two-sum.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        vector<int> v=nums,sol;
-        sort(nums.begin(),nums.end());
-        int j=nums.size()-1,i=0;
-        while( i < j)
-        {
-            if(nums[i]+nums[j]==target){break;}
-            if(nums[i]+nums[j]>target)j--;
-            else i++;
-        }
-        int a=2;
-        for(int k=0;k< v.size();k++)
-        {
-            if(a==0)break;
-            if(v[k]==nums[i]||nums[j]==v[k])
-            {
-                sol.push_back(k);
-                a--;
-            }
-        }
-        
-        return sol;
-        // int n= arr.size();
-        // unordered_map<int, int> mpp;
-        // for (int i = 0; i < n; i++)
+        // vector<int> v=nums,sol;
+        // sort(nums.begin(),nums.end());
+        // int j=nums.size()-1,i=0;
+        // while( i < j)
         // {
-        //     int num = arr[i];
-        //     int moreNeeded = target - num;
-        //     if (mpp.find(moreNeeded) != mpp.end()) {
-        //         return {mpp[moreNeeded], i};
-        //     }
-        //     mpp[num] = i;
+        //     if(nums[i]+nums[j]==target){break;}
+        //     if(nums[i]+nums[j]>target)j--;
+        //     else i++;
         // }
-        // return { -1, -1};
+        // int a=2;
+        // for(int k=0;k< v.size();k++)
+        // {
+        //     if(a==0)break;
+        //     if(v[k]==nums[i]||nums[j]==v[k])
+        //     {
+        //         sol.push_back(k);
+        //         a--;
+        //     }
+        // }
+        
+        // return sol;
+        int n= nums.size();
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            int moreNeeded = target - num;
+            if (mpp.find(moreNeeded) != mpp.end()) {
+                return {mpp[moreNeeded], i};
+            }
+            mpp[num] = i;
+        }
+        return { -1, -1};
         
     }
 };
