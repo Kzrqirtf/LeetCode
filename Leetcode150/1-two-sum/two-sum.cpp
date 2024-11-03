9dc6f3783aee55dbec2585c2d7d9ab96101ca261
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-11-03 22:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (51.50%) - LeetSync

--- Leetcode150/1-two-sum/two-sum.cpp
+++ Leetcode150/1-two-sum/two-sum.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        // vector<int> v=nums,sol;
-        // sort(nums.begin(),nums.end());
-        // int j=nums.size()-1,i=0;
-        // while( i < j)
-        // {
-        //     if(nums[i]+nums[j]==target){break;}
-        //     if(nums[i]+nums[j]>target)j--;
-        //     else i++;
-        // }
-        // int a=2;
-        // for(int k=0;k< v.size();k++)
-        // {
-        //     if(a==0)break;
-        //     if(v[k]==nums[i]||nums[j]==v[k])
-        //     {
-        //         sol.push_back(k);
-        //         a--;
-        //     }
-        // }
+        vector<int> v=nums,sol;
+        sort(nums.begin(),nums.end());
+        int j=nums.size()-1,i=0;
+        while( i < j)
+        {
+            if(nums[i]+nums[j]==target){break;}
+            if(nums[i]+nums[j]>target)j--;
+            else i++;
+        }
+        int a=2;
+        for(int k=0;k< v.size();k++)
+        {
+            if(a==0)break;
+            if(v[k]==nums[i]||nums[j]==v[k])
+            {
+                sol.push_back(k);
+                a--;
+            }
+        }
         
-        // return sol;
+        return sol;
         // int n= arr.size();
         // unordered_map<int, int> mpp;
         // for (int i = 0; i < n; i++)
@@ -35,21 +35,6 @@ public:
         //     mpp[num] = i;
         // }
         // return { -1, -1};
-        unordered_map<int, int> m;
-        vector<int> result;
-        for(int i = 0; i < nums.size(); i++){
-        // not found the second one
-            if (m.find(nums[i]) == m.end() ) {
-            // store the first one poisition into the second one's key
-                m[target - nums[i]] = i;
-            } else {
-            // found the second one
-                result.push_back(m[nums[i]]);
-                result.push_back(i);
-                break;
-            }
-        }
-        return result;
         
     }
 };
