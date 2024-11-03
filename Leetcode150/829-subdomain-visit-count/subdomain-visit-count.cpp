b829afa0f1191dd3058d7ac524e4d2ff1733b99d
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2024-11-03 10:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (33.07%) - LeetSync

--- Leetcode150/829-subdomain-visit-count/subdomain-visit-count.cpp
+++ Leetcode150/829-subdomain-visit-count/subdomain-visit-count.cpp
@@ -5,19 +5,14 @@ public:
         unordered_map<string, int> mp;
         for (auto dom : cpdomains) {
             int i = 0, num = 0;
-
-            // Parse the count at the beginning
             while (i < dom.size() && isdigit(dom[i])) {
                 num = num * 10 + (dom[i] - '0');
                 ++i;
             }
-            // Skip the space after the count
             ++i;
-
             // Now, `dom.substr(i)` will be the domain part
             string domain = dom.substr(i);
             mp[domain] += num;  // Add the full domain count
-
             // Process subdomains
             for (int j = 0; j < domain.size(); ++j) {
                 if (domain[j] == '.') {
@@ -29,7 +24,7 @@ public:
 
         // Prepare the result vector
         vector<string> ans;
-        for (const auto& [key, value] : mp) {
+        for (auto [key, value] : mp) {
             ans.push_back(to_string(value) + " " + key);
         }
 
