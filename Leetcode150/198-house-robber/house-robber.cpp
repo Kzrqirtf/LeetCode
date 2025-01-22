582c43c5dc967de14157703d4d4aba4665fbd1f5
Kritika Farswan <128362461+Kzrqirtf@users.noreply.github.com>
2025-01-22 18:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (58.76%) - LeetSync

--- Leetcode150/198-house-robber/house-robber.cpp
+++ Leetcode150/198-house-robber/house-robber.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int robb(vector<int>&nums,int i,int &ans,vector<int>&dp)
+    int robb(vector<int>&nums,int i,vector<int>&dp)
     {
         if(i>=nums.size())return 0;
         if(dp[i]!=-1)return dp[i];
         int pick1,pick2;
-        dp[i]=max(nums[i]+robb(nums,i+2,ans,dp),0+robb(nums,i+1,ans,dp));
+        dp[i]=max(nums[i]+robb(nums,i+2,dp),0+robb(nums,i+1,dp));
         return dp[i];
         
     }
@@ -14,7 +14,7 @@ public:
         vector<int>dp(nums.size()+1,-1);
         int i=0;
         int ans;
-        ans = robb(nums,i,ans,dp);
+        ans = robb(nums,i,dp);
         return ans;
 
         
